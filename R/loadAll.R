07daa807924e7ee0e188942cce19fd3b407347bc
wilsontom <wilsontom89@gmail.com>
2016-04-04 15:28:28 +0100

    add load and save funcs

---
+++ R/loadAll.R
@@ -0,0 +1,21 @@
+#' Load all xcmsSet's
+#'
+#' Load all of the xcmsSet's in the specified directory.
+#'
+#' @param path
+#' @return NULL
+#'
+#' @author Tom Wilson \email{tpw2@@aber.ac.uk}
+#' @export
+#' @seealso \link{saveAll}
+
+loadAll <- function(path)
+{
+  all_files <- list.files(path, pattern = ".RData")
+  for(i in 1:length(all_files)){
+    load(all_files[i])
+    varName <- gsub(".RData", "", all_files[i])
+    assign(eval(parse(text = "varName")), xset, envir = .GlobalEnv)
+  }
+  return(invisible(NULL))
+}
---
+++ R/saveAll.R
@@ -0,0 +1,21 @@
+#' Save all xcmsSet's
+#'
+#' Save all of the xcmsSet's currently in the global environment (\code{.GlobalEnv}) into the specified directory.
+#'
+#' @param path
+#' @return NULL
+#'
+#' @author Tom Wilson \email{tpw2@@aber.ac.uk}
+#' @export
+#' @seealso \link{loadAll}
+
+saveAll <- function(path)
+{
+  xsets <- Filter( function(x) 'xcmsSet' %in% class( get(x) ), ls() )
+  for(i in 1:length(xsets)){
+    file <- paste(path,paste(xsets[i], ".RData", sep = ""), sep = "/")
+    xset <- eval(parse(text = xsets[i]))
+    eval(parse(text=paste(save(xset, file = file))))
+  }
+  return(invisible(NULL))
+}
