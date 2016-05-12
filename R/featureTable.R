1184f447eeca129a509763d05e17e1da276c8145
wilsontom <wilsontom89@gmail.com>
2016-05-12 16:44:09 +0100

    append QCRSD and QCint

--- R/featureTable.R
+++ R/featureTable.R
@@ -11,33 +11,52 @@
 
 featureTable <- function(xcms_set)
 {
-  
+
   if(attr(xcms_set, "class")[1] != "xcmsSet"){
     stop("input must be an xcmsSet object")
   }
   if(length(xcms_set@groupidx) == 0){
     stop("...xcms set must contain peak group information")
   }
-  
+
   tab.tmp <- peakTable(xcms_set)
-  
+  peak_table <- formatTable(xcms_set)
+
   tab.tmp$npeaks <- NULL
-  
-  feat_info <- data.frame(cbind(tab.tmp$mz, tab.tmp$mzmin, tab.tmp$mzmax, tab.tmp$rt, tab.tmp$rtmin, tab.tmp$rtmax))
+
+  feat_info <- data.frame(tab.tmp$mz, tab.tmp$mzmin, tab.tmp$mzmax, tab.tmp$rt, tab.tmp$rtmin, tab.tmp$rtmax)
   names(feat_info) <- c("mz", "mzmin", "mzmax", "rt", "rtmin", "rtmax")
-  
+
   feat_info$rt <- round(feat_info$rt/60, digits = 2)
   feat_info$rtmin <- round(feat_info$rtmin/60, digits = 2)
   feat_info$rtmax <- round(feat_info$rtmax/60, digits = 2)
-  
+
   feat_rt_ran <- round(feat_info$rtmax - feat_info$rtmin, digits = 2)
   feat_mz_dppm <- round(((feat_info$mzmax - feat_info$mzmin) / feat_info$mz) * 1E06, digits = 2)
   feat_mz_damu <- round((feat_info$mzmax - feat_info$mzmin) / feat_info$mz, digits = 5)
-    
+
   feat_id <- paste("M", round(feat_info$mz, digits = 2) , "T", feat_info$rt, sep = "")
-  
+
   feat_tab <- data.frame(feat_id,feat_info, feat_rt_ran, feat_mz_damu, feat_mz_dppm)
   names(feat_tab)[1] <- "ID"
   names(feat_tab)[8:10] <- c("rt_range", "amu_dev", "ppm_dev")
+
+  if(any(xcms_set@phenoData$class == "QC")){
+    QCval <- metProc::CV(peak_table[which(xcms_set@phenoData$class == "QC"),])$CV
+  }
+  if(((any(xcms_set@phenoData$class == "QC")) == FALSE) & length(unique(xcms_set@phenoData$class) == 1)){
+    QCval <- metProc::CV(peak_table[which(xcms_set@phenoData$class == unique(xcms_set@phenoData$class)),])$CV
+  }
+
+  if(any(xcms_set@phenoData$class == "QC")){
+    table_tmp <- peak_table[which(xcms_set@phenoData$class == "QC"),]
+    QCme <- apply(table_tmp,2,mean)
+  }
+  if(((any(xcms_set@phenoData$class == "QC")) == FALSE) & length(unique(xcms_set@phenoData$class) == 1)){
+    QCme <- apply(peak_table,2,mean)
+  }
+
+  feat_tab <- data.frame(feat_tab, RSD = QCval, Int_mean = QCme)
+
   return(feat_tab)
-}
+  }
