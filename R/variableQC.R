f009c7746a6ff44e19bacaf60924e1668414590a
wilsontom <wilsontom89@gmail.com>
2016-05-12 16:43:54 +0100

    add more fields and checks

--- R/variableQC.R
+++ R/variableQC.R
@@ -24,14 +24,45 @@ variableQC <- function(xcms_set)
 
   QCres <- data.frame()
   QCres["Number of Features",1] <- ncol(peak_table)
-  QCres["% of Missing Values (MV)",1] <- (length(which(peak_table == 0)) / (ncol(peak_table) * nrow(peak_table))) * 100
-  QCres["% of Features with > 40 % MV",1] <- length(which(MV > 40)) / length(MV) * 100
-  QCres["Cor % MV ~ m/z",1] <- mvCmz
-  QCres["Cor % MV ~ Rt",1] <- mvCrt
-  QCres["Cor % MV ~ Mean Abundance",1] <- mvCint
-  QCres["Median CV (%)",1] <- metProc::CV(tab[which(xcms_set@phenoData$class == "QC"),])$CVmd
+  QCres["% of Missing Values (MV)",1] <- round((length(which(peak_table == 0)) / (ncol(peak_table) * nrow(peak_table))) * 100, digits = 2)
+  QCres["% of Features with > 40 % MV",1] <- round(length(which(MV > 40)) / length(MV) * 100, digits =2)
+  QCres["Cor % MV ~ m/z",1] <- round(mvCmz, digits = 2)
+  QCres["Cor % MV ~ Rt",1] <- round(mvCrt, digits = 2)
+  QCres["Cor % MV ~ Mean Abundance",1] <- round(mvCint, digits = 2)
+  if(any(xcms_set@phenoData$class == "QC")){
+    QCres["Median CV (%)",1] <- round(metProc::CV(peak_table[which(xcms_set@phenoData$class == "QC"),])$CVmd, digits = 2)
+  }
+  if(((any(xcms_set@phenoData$class == "QC")) == FALSE) & length(unique(xcms_set@phenoData$class) == 1)){
+    QCres["Median CV (%)",1] <- round(metProc::CV(peak_table[which(xcms_set@phenoData$class == unique(xcms_set@phenoData$class)),])$CVmd, digits = 2)
+  }
+  QCres["% of Features with RSD < 20%",1] <- round((length(which(metProc::CV(peak_table[which(xcms_set@phenoData$class == unique(xcms_set@phenoData$class)),])$CV <= 20)) / ncol(peak_table)) * 100, digits = 2)
+
+
+  QCres["Mean peak width (mins)",1] <- paste(round(mean(feat_table$rtmax - feat_table$rtmin),digits = 2),round(sd(feat_table$rtmax - feat_table$rtmin),digits = 2), sep = " +/- ")
+    QCres["Mean feature group error (ppm)",1] <- paste(round(mean((feat_table$mzmax - feat_table$mzmin) / feat_table$mz) * 1E06, digits = 2),
+            round(sd((feat_table$mzmax - feat_table$mzmin) / feat_table$mz) * 1E06, digits = 2), sep = " +/- ")
+
+
+  occ <- function(x)
+  {
+    ox <- length(which(x !=0)) / length(x)
+    return(ox)
+  }
+
+  if(((any(xcms_set@phenoData$class == "QC")) == FALSE) & length(unique(xcms_set@phenoData$class) == 1)){
+  occu <- sapply(data.frame(peak_table), function(x)(tapply(x, factor(xcms_set@phenoData$class), occ))) * 100
+  # % of total features which occupancy > 80% in QC class
+  occu80 <- (length(which(occu >= 80)) / length(occu)) * 100
+  }
+  if(any(xcms_set@phenoData$class == "QC")){
+  occu <- sapply(data.frame(peak_table), function(x)(tapply(x, factor(xcms_set@phenoData$class), occ))) * 100
+  occu <- data.frame(occu[which(rownames(occu) == "QC"),])
+  occu80 <- (length(which(occu[,1] >= 80)) / length(occu[,1])) * 100
+  }
+
+  QCres["% Features > 80 % Occupancy in QC",1] <- round(occu80, digits = 2)
+
   names(QCres)[1] <- "value"
 
-  QCres[,1] <- round(QCres[,1], digits = 2)
-
+  return(QCres)
   }
